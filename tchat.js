650994150d05292b656145586ee9f382b2a93872
darkontes1 <darkontes@hotmail.fr>
2015-04-07 11:45:00 +0200

    raa

--- tchat.js
+++ tchat.js
@@ -1,22 +1,14 @@
 //Quand on click sur le bouton du formulaire "ok"
 $(document).on("click","#ok",function(e){
-<<<<<<< HEAD
-    e.preventDefault();
-=======
     //Supprime la fonction par défaut de l'event form
     e.preventDefault();
     //Récupère la variable du message à ajouter
->>>>>>> origin/master
     var message = $("#message").val();
     //alert(message);
     $.ajax({
         method:"POST",
-<<<<<<< HEAD
         url:"tchat.html",
         //data:{},
-=======
-        url:"tchat.php",
->>>>>>> origin/master
         success:function(r){
             //$("container").html($(r).find("container").html());
             /*var html = "";
