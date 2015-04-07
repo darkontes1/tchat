a51b395a1ae03acd9af11d34fdf33e2d799903d0
darkontes1 <darkontes@hotmail.fr>
2015-04-07 11:42:45 +0200

    tatatatata

--- tchat.js
+++ tchat.js
@@ -1,15 +1,18 @@
+//Quand on click sur le bouton du formulaire "ok"
 $(document).on("click","#ok",function(e){
+    //Supprime la fonction par défaut de l'event form
+    e.preventDefault();
+    //Récupère la variable du message à ajouter
     var message = $("#message").val();
-    alert(message);
+    //alert(message);
     $.ajax({
         method:"POST",
         url:"tchat.php",
-        data:{},
         success:function(r){
             //$("container").html($(r).find("container").html());
-            var html = "";
+            /*var html = "";
             var toto = eval(r);
-            alert(toto);
+            alert(toto);*/
         }
     });
 });
