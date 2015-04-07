2a9fb42045e05d20d7cb4ae67f5b1e2f3cd9fd0e
berrabahs <sofiane_berrabah@hotmail.fr>
2015-04-07 11:43:24 +0200

    higoi

--- tchat.js
+++ tchat.js
@@ -1,10 +1,11 @@
 $(document).on("click","#ok",function(e){
+    e.preventDefault();
     var message = $("#message").val();
     alert(message);
     $.ajax({
         method:"POST",
-        url:"tchat.php",
-        data:{},
+        url:"tchat.html",
+        //data:{},
         success:function(r){
             //$("container").html($(r).find("container").html());
             var html = "";
