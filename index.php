9efe119a5a798b296d926e7816eed45a37a76582
darkontes1 <darkontes@hotmail.fr>
2015-04-09 10:12:17 +0200

    tata

--- index.php
+++ index.php
@@ -22,30 +22,27 @@ session_start();
         <div id="false">
             <form id="connex" method="POST" action="verifco.php">
                 <p>CONNEXION</p>
-                <label>Pseudo : </label><input type="text" id="valueCo" name="valueCo" required><br/>
-                <label>Password : </label><input type="password" id="passCo" name="passCo" required><br/>
-                <input type="submit" id="co" name="co" value="connection"/>
+                <label>Pseudo : </label><input class="in_" type="text" id="valueCo" name="valueCo" required><br/>
+                <label>Password : </label><input class="in_" type="password" id="passCo" name="passCo" required><br/>
+                <input class="bout" type="submit" id="co" name="co" value="connection"/>
             </form>
-            <form id="insc" method="POST" action="inscription.php">
+            <form id="inscription" method="POST" action="inscription.php">
                 <p>INSCRIPTION</p>
-                <label>Pseudo : </label><input type="text" id="valueIns" name="valueInsc" required><br/>
-                <label>Password : </label><input type="password" id="passIns" name="passIns" required><br/>
-                <label>Verif Password : </label><input type="password" id="passVeri" name="passVeri" required><br/>
-
-                <input type="submit" id="insc" name="insc" value="inscription"/>
-
+                <label>Pseudo : </label><input class="in_" type="text" id="valueIns" name="valueInsc" required><br/>
+                <label>Password : </label><input class="in_" type="password" id="passIns" name="passIns" required><br/>
+                <label>Verif Password : </label><input class="in_" type="password" id="passVeri" name="passVeri" required><br/>
+                <input class="bout" type="submit" id="insc" name="insc" value="inscription"/>
             </form>
         </div>
             <div id="true">
                 <form id="aff" method="POST" action="tchat.php">
-                
                 <div class="div_t">pseudo
-                    <input type="text" name="user" id="user" value="<?php echo $_SESSION['pseudo'] ?>" />
-                    <button id="deco" name="deco">deconnection</button>
+                    <input class="in_" type="text" name="user" id="user" value="<?php echo $_SESSION['pseudo'] ?>" />
+                    <button class="bout" id="deco" name="deco">deconnection</button>
                 </div>
                 <div class="div_t"><textarea id="tchat" rows="20" cols="100"></textarea></div>
                 <div class="div_t"><textarea id="message" name="message" rows="3" cols="100" ></textarea></div>
-                <input type="submit" name="ok" id="ok" />
+                <input class="bout" type="submit" name="ok" id="ok" />
             </form>
         </div>
     </body>
