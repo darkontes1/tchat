bb85e17648c2dbda7fa62851bfa8c4cb8c2cb36d
darkontes1 <darkontes@hotmail.fr>
2015-04-09 10:25:51 +0200

    titi

--- index.php
+++ index.php
@@ -21,28 +21,30 @@ session_start();
     <body>
         <div id="false">
             <form id="connex" method="POST" action="verifco.php">
-                <p>CONNEXION</p>
+                <h3>CONNEXION</h3>
                 <label>Pseudo : </label><input class="in_" type="text" id="valueCo" name="valueCo" required><br/>
                 <label>Password : </label><input class="in_" type="password" id="passCo" name="passCo" required><br/>
                 <input class="bout" type="submit" id="co" name="co" value="connection"/>
             </form>
             <form id="inscription" method="POST" action="inscription.php">
-                <p>INSCRIPTION</p>
+                <h3>INSCRIPTION</h3>
                 <label>Pseudo : </label><input class="in_" type="text" id="valueIns" name="valueInsc" required><br/>
                 <label>Password : </label><input class="in_" type="password" id="passIns" name="passIns" required><br/>
                 <label>Verif Password : </label><input class="in_" type="password" id="passVeri" name="passVeri" required><br/>
                 <input class="bout" type="submit" id="insc" name="insc" value="inscription"/>
             </form>
         </div>
-            <div id="true">
-                <form id="aff" method="POST" action="tchat.php">
-                <div class="div_t">pseudo
+        <div id="true">
+            <form id="aff" method="POST" action="tchat.php">
+                <div class="div_t toto">pseudo
                     <input class="in_" type="text" name="user" id="user" value="<?php echo $_SESSION['pseudo'] ?>" />
-                    <button class="bout" id="deco" name="deco">deconnection</button>
+                    <button class="bout2 bout" id="deco" name="deco">deconnection</button>
                 </div>
-                <div class="div_t"><textarea id="tchat" rows="20" cols="100"></textarea></div>
-                <div class="div_t"><textarea id="message" name="message" rows="3" cols="100" ></textarea></div>
-                <input class="bout" type="submit" name="ok" id="ok" />
+                <div id="titi">
+                    <div class="div_t"><textarea id="tchat" rows="20" cols="100"></textarea></div>
+                    <div class="div_t"><textarea id="message" name="message" rows="3" cols="100" ></textarea></div>
+                    <input class="bout2 bout" type="submit" name="ok" id="ok" />
+                </div>
             </form>
         </div>
     </body>
